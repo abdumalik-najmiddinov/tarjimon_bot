aae617be24547b92ed16aa63dbf0903980b37a1e
abdumalik-najmiddinov <abdumalik77770@gmail.com>
2025-07-12 11:09:34 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message
+from googletrans import Translator
+
+API_TOKEN = "8117810778:AAGJQi6rhDnVQVlPJKDmOzkiw7CYCFptiao"
+
+lists = ["ahmoq", "telba", "jinni", "tupoy"]
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+translator = Translator()
+
+
+@dp.message(Command("start"))
+async def start_handler(message: Message):
+    name = message.from_user.full_name
+    await message.answer(
+        f"✋ <b>{name}! Asalomu Alaykum!\nMen tarjimon botman, matn kiriting, tarjima qilib beraman.</b>"
+        , parse_mode="html")
+
+
+@dp.message(F.text.func(lambda text: text is not None and any(word in text.lower().split() for word in lists)))
+async def handle_insult(message: Message):
+    await message.reply("Iltimos, odobli bo'ling!")
+
+
+@dp.message()
+async def translate_handler(message: Message):
+    text = message.text
+    tarjima = translator.translate(text, src='auto', dest='en')
+    await message.answer(tarjima.text)
+
+
+async def on_start_up(dispatcher: Dispatcher):
+    await bot.send_message(chat_id="7034143307",
+                           text="Bot ishga tushdi")
+
+
+async def main():
+    dp.startup.register(on_start_up)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
